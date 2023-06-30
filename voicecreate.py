da64e037f8d7f5b444c1a36c3d8972d15e19d156
Dmitriy <109019828+alkyqekasy@users.noreply.github.com>
2023-06-30 16:14:38 +0700

    Update voicecreate.py

--- voicecreate.py
+++ voicecreate.py
@@ -11,13 +11,13 @@ bot = commands.Bot(command_prefix=".", intents=intents)
 
 bot.remove_command("help")
 
-DISCORD_TOKEN = 'Enter Discord Token here'
+DISCORD_TOKEN = 'MTA5NDMxNTkyMTgxODAxMzc0Ng.GG4ZAd.scbGvHzhquXv6ocPQgJIVOSNK52EfIDOlRw4r8'
 
 initial_extensions = ['cogs.voice']
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
+    print('Вы вошли как')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
@@ -25,9 +25,9 @@ async def on_ready():
     for extension in initial_extensions:
         try:
             await bot.load_extension(extension)
-            print(f'Loaded {extension}')
+            print(f'Загружено {extension}')
         except Exception as e:
-            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            print(f'Не удалось загрузить расширение {extension}.', file=sys.stderr)
             traceback.print_exc()
 
 bot.run(DISCORD_TOKEN)
